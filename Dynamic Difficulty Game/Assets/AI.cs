919ea2c3370c317de648fe68b01ff8aa4faf00a1
Mathias Esmann <maemdk@gmail.com>
2018-04-11 09:15:45 +0200

    Ai WIP 1
    Added simple AI with patrol and Follow function

---
+++ Dynamic Difficulty Game/Assets/AI.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AI : MonoBehaviour {
+
+	NavMeshAgent agent;
+	public bool patrol;
+	public bool chasePlayer;
+	public GameObject patrolWaypointHandler;
+	int currentWaypoint;
+	GameObject player;
+
+	void Start(){
+		agent = GetComponent<NavMeshAgent>();
+		agent.autoBraking = false;
+		player = GameObject.FindGameObjectWithTag("Player");
+	}
+
+	void Update(){
+		if (patrol && agent.remainingDistance < 0.5f)
+		{
+			GoToNextWaypoint();
+		}
+
+		if (chasePlayer)
+		{
+			if (patrol == true)
+				patrol = false;
+
+			agent.destination = player.transform.position;
+		}
+	}
+
+	void GoToNextWaypoint(){
+		agent.destination = patrolWaypointHandler.transform.GetChild(currentWaypoint).position;
+
+		currentWaypoint = (currentWaypoint + 1) % patrolWaypointHandler.transform.childCount;
+	}
+
+}

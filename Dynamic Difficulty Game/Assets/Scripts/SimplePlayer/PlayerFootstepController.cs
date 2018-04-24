f950f7faece74543bca383297ba7ea8e2f66bc37
Mathias Esmann <maemdk@gmail.com>
2018-04-24 22:57:57 +0200

    Prototype 0.1
    The game is in a playable state, with super simple AI and Player mechanics.

---
+++ Dynamic Difficulty Game/Assets/Scripts/SimplePlayer/PlayerFootstepController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerFootstepController : MonoBehaviour {
+
+	public float strideLenght = 0.3f;
+	public AudioClip footstepSound;
+	[Range(0,1)]
+	public float stepVolume = 0.5f;
+	public float stepAiVolume = 70f;
+	
+	Vector3 currentPosition;
+	Vector3 lastStepPos;
+
+	AudioSource audioSource;
+
+	void Start(){
+		lastStepPos = transform.position;
+		audioSource = GetComponent<AudioSource>();
+	}
+
+	void FixedUpdate(){
+		currentPosition = transform.position;
+	}
+
+	void Update(){
+		float distance = (currentPosition - lastStepPos).magnitude;
+		if (distance > strideLenght)
+		{
+			MakeFootstep();
+		}
+	}
+
+	void MakeFootstep(){
+		lastStepPos = currentPosition;
+		audioSource.PlayOneShot(footstepSound, stepVolume);
+
+		//Make AI agent withing X range check out footstep sound
+		GameObject[] aiAgents = GameObject.FindGameObjectsWithTag("AI");
+		foreach (GameObject agent in aiAgents)
+		{
+			float distance = (transform.position - agent.transform.position).magnitude;
+			if (distance < stepVolume * stepAiVolume)
+			{
+				agent.GetComponent<AIController>().chasePlayer = true;
+			}
+		}
+	}
+
+}
--- Dynamic Difficulty Game/Assets/Scripts/SimplePlayer/SimplePistol.cs
+++ Dynamic Difficulty Game/Assets/Scripts/SimplePlayer/SimplePistol.cs
@@ -5,7 +5,17 @@ using UnityEngine;
 public class SimplePistol : MonoBehaviour {
 
 	public GameObject bullet;
+	public AudioClip fireSound;
+	[Range(0,1)]
+	public float fireVolume = 1f;
+	public float aiHearRange = 10f;
 	public float bulletSpeed = 5f;
+
+	AudioSource audioSource;
+
+	void Start(){
+		audioSource = GetComponent<AudioSource>();
+	}
 
 	void Update(){
 		if (Input.GetKeyDown(KeyCode.Mouse0))
@@ -19,6 +29,17 @@ public class SimplePistol : MonoBehaviour {
 		Rigidbody rb = _bullet.GetComponent<Rigidbody>();
 		rb.AddForce(_bullet.transform.forward * bulletSpeed * 10);
 		Physics.IgnoreCollision(_bullet.GetComponent<Collider>(), GetComponent<Collider>());
-	}
+		audioSource.PlayOneShot(fireSound, fireVolume);
 
+		//Make all AIs chase player
+		GameObject[] aiAgents = GameObject.FindGameObjectsWithTag("AI");
+		foreach (GameObject agent in aiAgents)
+		{
+			float distance = (transform.position - agent.transform.position).magnitude;
+			if (distance < aiHearRange)
+			{
+				agent.GetComponent<AIController>().chasePlayer = true;
+			}
+		}
+	}
 }
